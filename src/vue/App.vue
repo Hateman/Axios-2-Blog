624d8338dd0e08061e97b8538ab8a54aaf0bdc2c
Alex <37542088+Hateman@users.noreply.github.com>
2019-04-14 18:05:00 +0300

    Add files via upload

---
+++ src/vue/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <!-- <router-view></router-view> -->
+  <app-content></app-content>
+</div>
+</template>
+
+<script>
+import Content from './Content.vue';
+// import VueRouter from 'vue-router';
+
+export default {
+  components: {
+    'app-content': Content,
+    // 'router-view': VueRouter,
+  },
+}
+
+</script>
+
+<style></style>
---
+++ src/vue/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style src="../CSS.css"></style>
