1b84898a48fb30693dc416cd96bef975614d0270
Alex <37542088+Hateman@users.noreply.github.com>
2019-04-15 00:46:40 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
 
 import Vue from 'vue'
-import App from './vue/App.vue'
 import VueRouter from 'vue-router'
+import App from './vue/App.vue'
+import Content from './vue/Content.vue'
+import Detailed from './vue/Detailed.vue'
+
 
 // var Vue = require ('vue')
 // var VueRouter = require ('vue-router')
 // var App = require ('./vue/App.vue')
-// var Content = require ('./vue/Content.vue')
 // var Pagination = require ('./vue/Pagination.vue')
-var Detailed = require ('./vue/Detailed.vue')
 // var Header = require ('./vue/Header.vue')
+// var Content = require ('./vue/Content.vue')
+// var Detailed = require ('./vue/Detailed.vue')
 
 Vue.use(VueRouter)
 
 var router = new VueRouter ({
     routes: [
         { path: '/detailed', component: Detailed },
+        { path: '/', component: Content },
     ]
 })
 
